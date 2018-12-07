2c0b42be340c8d8116fb6c4c023b79ec7b6a76fc
Johannes <mail@johannez.com>
2018-12-07 15:35:05 -0800

    Added project6 conf plugin

---
+++ wp-content/mu-plugins/p6-conf/p6-conf.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Project6 Configuration
+ * Description: Global site configuration.
+ * Version: 0.1.0
+ * Author: Johannes Schmidt
+ * Author URI: https://www.project6.com
+ */
+
+d('hello');
